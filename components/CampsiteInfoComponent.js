0bc060f0477e79a6c573ede58de9736173b42e36
Evan Longwood <evanmlongwood@gmail.com>
2020-05-26 14:59:10 -0700

    Navigation.

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { Component } from "react";
 import { Text, View } from "react-native";
 import { Card } from "react-native-elements";
+import { Campsites, CAMPSITES } from "../shared/campsites";
 
 function RenderCampsite({ campsite }) {
   if (campsite) {
@@ -16,8 +17,24 @@ function RenderCampsite({ campsite }) {
   return <View />;
 }
 
-function CampsiteInfo(props) {
-  return <RenderCampsite campsite={props.campsite} />;
+class CampsiteInfo extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      campsites: CAMPSITES,
+    };
+  }
+
+  static navigationOptions = {
+    title: "Campsite Information",
+  };
+  render() {
+    const campsiteId = this.props.navigation.getParam("campsiteId");
+    const campsite = this.state.campsites.filter(
+      (campsite) => campsite.id === campsiteId
+    )[0];
+    return <RenderCampsite campsite={campsite} />;
+  }
 }
 
 export default CampsiteInfo;
