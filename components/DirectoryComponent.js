708a17b464a72316475ca6af56b329e6aa2fd60a
Evan Longwood <evanmlongwood@gmail.com>
2020-05-25 21:00:44 -0700

    first commit

---
+++ components/DirectoryComponent.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { FlatList } from "react-native";
+import { ListItem } from "react-native-elements";
+
+function Directory(props) {
+  const renderDirectoryItem = ({ item }) => {
+    return (
+      <ListItem
+        title={item.name}
+        subtitle={item.description}
+        leftAvatar={{ source: require("./images/react-lake.jpg") }}
+      />
+    );
+  };
+  return (
+    <FlatList
+      data={props.campsites}
+      renderItem={renderDirectoryItem}
+      keyExtrator={(item) => item.id.toString()}
+    />
+  );
+}
+
+export default Directory;
